9523fdfcd924d88715dc4abe2d7f07ac7a3c79cb
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-12-13 11:16:47 +0100

    test happy path in user_sessions and static pages

--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -1,11 +1,8 @@
 require "test_helper"
 
 class UserSessionsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user_session = user_sessions(:one)
-  end
-
   test "should get index" do
+    login
     get user_sessions_url
     assert_response :success
   end
@@ -15,34 +12,49 @@ class UserSessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get login" do
+    get login_url
+    assert_response :success
+  end
+
   test "should create user_session" do
     assert_difference('UserSession.count') do
-      post user_sessions_url, params: { user_session: { name: @user_session.name } }
+      username = Rails.application.credentials.auth[:login]
+      password = Rails.application.credentials.auth[:password]
+      post user_sessions_url(username: username, password: password, name: 'Test Session')
     end
 
-    assert_redirected_to user_session_url(UserSession.last)
+    assert_redirected_to root_path
   end
 
   test "should show user_session" do
-    get user_session_url(@user_session)
+    login
+    user_session = UserSession.last
+    get user_session_url(user_session)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_user_session_url(@user_session)
+    login
+    user_session = UserSession.last
+    get edit_user_session_url(user_session)
     assert_response :success
   end
 
   test "should update user_session" do
-    patch user_session_url(@user_session), params: { user_session: { name: @user_session.name } }
-    assert_redirected_to user_session_url(@user_session)
+    login
+    user_session = UserSession.last
+    patch user_session_url(user_session), params: { user_session: { name: "Other" } }
+    assert_redirected_to user_session_url(user_session)
   end
 
   test "should destroy user_session" do
+    login
+    user_session = UserSession.last
     assert_difference('UserSession.count', -1) do
-      delete user_session_url(@user_session)
+      delete user_session_url(user_session)
     end
 
-    assert_redirected_to user_sessions_url
+    assert_redirected_to root_path
   end
 end
