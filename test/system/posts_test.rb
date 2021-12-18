bc61ca16e48166c397579f99a3ab52497e340b83
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-12-18 16:03:00 +0100

    fix posts system tests

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -2,40 +2,59 @@ require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
   setup do
-    @post = posts(:one)
+    @post = posts(:first_post)
+  end
+
+  def sign_in
+    visit login_url
+
+    fill_in "username", with: Rails.application.credentials.auth[:login]
+    fill_in "password", with: Rails.application.credentials.auth[:password]
+    fill_in "name", with: "Test"
+
+    click_button "Login"
   end
 
   test "visiting the index" do
     visit posts_url
-    assert_selector "h1", text: "Posts"
+    assert_selector "h2:last-of-type", text: "📨 Latest Posts"
   end
 
   test "creating a Post" do
+    sign_in
+
     visit posts_url
-    click_on "New Post"
+    click_on "Create new Post"
 
     fill_in "Published at", with: @post.published_at
     fill_in "Title", with: @post.title
     click_on "Create Post"
 
     assert_text "Post was successfully created"
-    click_on "Back"
+    click_on "Christophers thoughts"
   end
 
   test "updating a Post" do
-    visit posts_url
-    click_on "Edit", match: :first
+    sign_in
 
-    fill_in "Published at", with: @post.published_at
+    visit posts_url
+    click_on @post.title
+    click_on "Edit"
+
     fill_in "Title", with: @post.title
     click_on "Update Post"
 
     assert_text "Post was successfully updated"
-    click_on "Back"
+    click_on "Christophers thoughts"
   end
 
   test "destroying a Post" do
+    sign_in
+
     visit posts_url
+    click_on @post.title
+    click_on "Edit"
+
     page.accept_confirm do
       click_on "Destroy", match: :first
     end
