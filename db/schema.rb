a28659d76a9f52fd7b44eb9ed2a716a6df908e8d
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-01-11 02:21:36 +0100

    add unique constraint to tags name index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_152157) do
+ActiveRecord::Schema.define(version: 2022_01_11_011939) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2022_01_07_152157) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_tags_on_name"
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "user_sessions", force: :cascade do |t|
