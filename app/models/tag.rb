33780ec5ae48d1088942fa821eb0a92109e9e1d2
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-01-11 02:42:19 +0100

    fix tags tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,12 @@
 class Tag < ApplicationRecord
-  validates :name, format: { with: /\A[a-z0-9][a-z0-9\-_]*[a-z0-9]\z/ }
+  validates :name, format: {
+    with: /\A[a-z0-9][a-z0-9\-_]*[a-z0-9]\z/,
+    message: "can only include a-z, 0-9, - and _"
+  }
 
   has_and_belongs_to_many :posts
 
-  default_scope { order(:name) }
+  default_scope { order :name }
 
   def to_param
     name
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create Tag" do
+    tag = Tag.new
+    tag.name = "tests"
+
+    assert tag.save
+  end
+
+  test "should find tag" do
+    tag_id = tags(:bands).id
+    assert_nothing_raised { Tag.find tag_id }
+  end
+
+  test "should validate tag name" do
+    tag = Tag.new
+    tag.name = "Tests"
+
+    assert_not tag.save
+    assert_equal tag.errors.first.full_message, "Name can only include a-z, 0-9, - and _"
+  end
+
+  test "should not create a tag with a not unique name" do
+    tag = Tag.new name: tags(:programming).name
+    assert_raise(ActiveRecord::RecordNotUnique) { tag.save }
+  end
 end
