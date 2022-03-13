4e3d2b8775d9b25ba39c9f1b8ed3a24842afa7f5
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-03-14 00:42:58 +0100

    basic WebMention support

--- app/jobs/webmention_job.rb
+++ app/jobs/webmention_job.rb
@@ -1,7 +1,63 @@
 class WebmentionJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  class DisallowedHost < StandardError; end
+
+  discard_on DisallowedHost
+
+  def perform(source:, target:)
+    return if source.nil? || target.nil? || source.empty? || target.empty?
+    @source = source
+    @target = URI(target)
+    raise DisallowedHost unless check_uri @target
+
+    webmention_endpoint = fetch_endpoint
+    raise DisallowedHost if webmention_endpoint.nil?
+
+    post_mention_to webmention_endpoint
+  end
+
+  private
+
+  # Fetch the target url and extracts the WebMention Endpoint from it.
+  def fetch_endpoint
+    res = fetch_doc
+
+    # first check for the endpoint in the link header
+    uri = webmention_from_header res
+    return uri unless uri.nil?
+
+    # if not then look in the body
+    parsed_data = Nokogiri::HTML.parse res.body
+    # Get the first a or link with rel="webmention"
+    element = parsed_data.at_css 'a[rel="webmention"], link[rel="webmention"]'
+    return nil if element.nil?
+
+    uri = URI element.attr('href')
+    return uri if check_uri uri
+  end
+
+  # Extract the WebMention uri is in a link header and extract it.
+  def webmention_from_header(res)
+    return if res.header[:Link].nil?
+    return unless res.header[:Link].include?('rel="webmention"')
+    match = /(?<=<).*(?=>)/.match(res.header[:Link])
+    uri = URI(match[0])
+    return uri if check_uri uri
+  end
+
+  def fetch_doc
+    Net::HTTP.get_response @target
+  end
+
+  # Post the WebMention to the endpoint.
+  # The format is from https://www.w3.org/TR/webmention/#sender-notifies-receiver
+  def post_mention_to(endpoint)
+    res = Net::HTTP.post_form endpoint, [["source", @source], ["target", @target]]
+  end
+
+  # Check if the uri is not nil, and its host is not nil nor localhost
+  def check_uri(uri)
+    !uri.nil? && !uri.host.nil? && uri.host != 'localhost'
   end
 end
--- test/jobs/webmention_job_test.rb
+++ test/jobs/webmention_job_test.rb
@@ -3,18 +3,18 @@ require "webmock/minitest"
 
 class WebmentionJobTest < ActiveJob::TestCase
   def source
-    "https://local-blog.de/post/test"
+    "http://local-blog.de/post/test"
   end
 
   test "that it fetches the passed URI and post to its webmention endpoint" do
-    stub_request(:any, "tester.io").to_return(
+    stub_request(:any, /tester\.io/).to_return(
       {
         headers: { Link: '<http://tester.io/webmention-endpoint>; rel="webmention"' },
         body: "<html><head></head><body></body></html>"
       },
       { status: 202 }
     )
-    target = "https://tester.io/article/1"
+    target = "http://tester.io/article/1"
 
     WebmentionJob.perform_now(source: source, target:)
 
@@ -22,15 +22,17 @@ class WebmentionJobTest < ActiveJob::TestCase
     assert_requested(
       :post,
       'http://tester.io/webmention-endpoint',
-      headers: { 'Content-Type' => "application/x-www-form-urlencoded" }
-    ) do |req|
-      req.body.source == source && req.body.target == target
-    end
+      headers: { 'Content-Type' => "application/x-www-form-urlencoded" },
+      body: {
+        :source => source,
+        :target => target
+      }
+    )
   end
 
   test "that it does not fetch localhost" do
     stub_request(:any, "localhost")
-    target = "https://localhost/article/1"
+    target = "http://localhost/article/1"
 
     WebmentionJob.perform_now(source: source, target:)
 
@@ -39,7 +41,7 @@ class WebmentionJobTest < ActiveJob::TestCase
 
   test "that it does not post to a localhost webmention endpoint" do
     endpoint = "http://localhost/webmention-endpoint"
-    stub_request(:any, "tester.io").to_return(
+    stub_request(:any, /tester\.io/).to_return(
       {
         headers: { Link: "<#{endpoint}>; rel=\"webmention\"" },
         body: "<html><head></head><body></body></html>"
@@ -54,7 +56,7 @@ class WebmentionJobTest < ActiveJob::TestCase
   end
 
   test "that it does get it fallsback to the link in the body" do
-    stub_request(:any, "tester.io").to_return(
+    stub_request(:any, /tester\.io/).to_return(
       {
         body: <<~HTML
         <html>
@@ -67,18 +69,19 @@ class WebmentionJobTest < ActiveJob::TestCase
       },
       { status: 202 }
     )
-    target = "https://tester.io/article/1"
+    target = "http://tester.io/article/1"
 
     WebmentionJob.perform_now(source: source, target:)
 
     assert_requested :get, target
-    assert_requested :post, 'http://tester.io/webmention-endpoint' do |req|
-      req.body.source == source && req.body.target == target
-    end
+    assert_requested :post, 'http://tester.io/webmention-endpoint', body: {
+      :source => source,
+      :target => target
+    }
   end
 
   test "that it does get it fallsback to the anchor in the body" do
-    stub_request(:any, "tester.io").to_return(
+    stub_request(:any, /tester\.io/).to_return(
       {
         body: <<~HTML
         <html>
@@ -91,18 +94,19 @@ class WebmentionJobTest < ActiveJob::TestCase
       },
       { status: 202 }
     )
-    target = "https://tester.io/article/1"
+    target = "http://tester.io/article/1"
 
     WebmentionJob.perform_now(source: source, target:)
 
     assert_requested :get, target
-    assert_requested :post, 'http://tester.io/webmention-endpoint' do |req|
-      req.body.source == source && req.body.target == target
-    end
+    assert_requested :post, 'http://tester.io/webmention-endpoint', body: {
+      :source => source,
+      :target => target
+    }
   end
 
   test "that only the first link or anchor is used" do
-    stub_request(:any, "tester.io").to_return(
+    stub_request(:any, /tester\.io/).to_return(
       {
         body: <<~HTML
         <html>
@@ -119,7 +123,7 @@ class WebmentionJobTest < ActiveJob::TestCase
       { status: 202 }
     )
 
-    WebmentionJob.perform_now(source: source, target: "https://tester.io/article/1")
+    WebmentionJob.perform_now(source: source, target: "http://tester.io/article/1")
 
     assert_requested :post, "http://tester.io/webmention-endpoint"
     assert_not_requested :post, "http://tester.io/other-endpoint"
