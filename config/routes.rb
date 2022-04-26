27e93ff2f7aab2761ccad8575c44081d0ec95133
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-04-26 21:03:19 +0200

    add bookmark scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
 
+  resources :bookmarks
   resources :posts, param: :slug
   resources :tags, param: :name, only: [:index, :show]
   resources :user_sessions
