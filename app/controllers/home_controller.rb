229a3e9d09d3012b8013d04e33ac80fea1c17907
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-05-07 17:32:10 +0200

    add sql union query to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,30 @@
 class HomeController < ApplicationController
   def index
-    posts = Post.published.with_rich_text_body.limit 10
-    bookmarks = Bookmark.all.limit 10
-    @entries = (posts + bookmarks).sort_by { |entry| entry.published_at }
-    @entries.reverse!
+    sorted_entries = ActiveRecord::Base.connection.exec_query ApplicationRecord.sanitize_sql([
+      %{
+        (
+          SELECT bookmarks.id, 'bookmark' as type, created_at as date FROM bookmarks
+          UNION
+          SELECT posts.id, 'post' as type, published_at as date FROM posts
+        ) ORDER BY date desc LIMIT 20 OFFSET :offset
+      },
+      { offset: 0 }
+    ])
+
+    posts = Post.with_rich_text_body.find(filter_entries(sorted_entries, "post")).group_by(&:id)
+    bookmarks = Bookmark.find(filter_entries(sorted_entries, "bookmark")).group_by(&:id)
+
+    @entries = sorted_entries.map &(lambda do |info|
+      id = info["id"]
+      model_type = info["type"]
+      return bookmarks[id].first if model_type == 'bookmark'
+      return posts[id].first if model_type == 'post'
+    end)
   end
+
+  private
+    def filter_entries(entries, type)
+      filter = lambda { |entry| entry["id"] if entry["type"] == type }
+      entries.filter_map &filter
+    end
 end
