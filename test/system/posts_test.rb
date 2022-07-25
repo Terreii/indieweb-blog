5913d8dbf8700575000e1fe720ad26bd589def3e
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-07-25 23:39:52 +0200

    add thumbnail file to post fixtures

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -30,6 +30,17 @@ class PostsTest < ApplicationSystemTestCase
     assert_equal find(".trix-content").native.attribute('outerHTML').strip, @post.body.to_s.strip
   end
 
+  test "Post should set Open Graph meta tags" do
+    visit post_url @post
+
+    assert_css "meta[property=\"og:title\"][content*=\"#{@post.title}\"]", visible: false
+    assert_css "meta[property=\"og:type\"][content*=article]", visible: false
+    assert_css "meta[property=\"og:description\"][content*=\"#{@post.summary}\"]", visible: false
+    assert_css "meta[property=\"og:image\"][content*=\"#{url_for @post.thumbnail}\"]", visible: false
+    assert_css "meta[property=\"og:url\"][content*=\"#{post_url @post}\"]", visible: false
+    assert_css "meta[property=\"twitter:card\"][content*=\"summary_large_image\"]", visible: false
+  end
+
   test "creating a Post" do
     login
 
