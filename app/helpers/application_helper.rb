f118ba58afc59c89df73ec48126714983abaff97
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-09-15 16:01:37 +0200

    add thumbnail fallback

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,11 @@ module ApplicationHelper
 
   def set_meta_data(description:, thumbnail:, url:)
     content_for(:description, description) unless description.nil?
-    content_for(:thumbnail, full_url_for(thumbnail)) unless thumbnail.nil?
+    content_for(:thumbnail, thumbnail.nil? ? gravatar_url : full_url_for(thumbnail))
     content_for(:share_url, url || request.original_url)
+  end
+
+  def gravatar_url
+    "https://www.gravatar.com/avatar/#{Rails.application.credentials.gravatar}"
   end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -11,4 +11,28 @@ class ApplicationHelperTest < ActionView::TestCase
     set_title(alignment)
     assert_dom_equal "#{alignment} | #{t('general.title')}", content_for(:title)
   end
+
+  test "set_meta_data should render the meta data" do
+    post = posts :first_post
+    picture = post.thumbnail
+    set_meta_data(description: "Test page", thumbnail: picture, url: url_for(post))
+
+    assert_equal "Test page", content_for(:description)
+    assert_equal full_url_for(picture), content_for(:thumbnail)
+    assert_equal url_for(post), content_for(:share_url)
+  end
+
+  test "set_meta_data should use the request url if url is nil" do
+    post = posts :first_post
+    set_meta_data description: "Test page", thumbnail: post.thumbnail, url: nil
+
+    assert_equal "http://test.host", content_for(:share_url)
+  end
+
+  test "set_meta_data should use a default thumbnail if it is nil" do
+    post = posts :first_post
+    set_meta_data description: "Test page", thumbnail: nil, url: url_for(post)
+
+    assert_equal gravatar_url, content_for(:thumbnail)
+  end
 end
