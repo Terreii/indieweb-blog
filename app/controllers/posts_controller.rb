80a6b76399ecbda90cd51bd730013f2ee093f1fa
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-01-14 02:52:50 +0100

    rename @post to @entry

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   # GET /posts/slug or /posts/slug.json
   def show
-    unless @post.published? || logged_in?
+    unless @entry.published? || logged_in?
       access_denied
     end
   end
@@ -29,15 +29,15 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Entry.create_with_post(post_params)
+    @entry = Entry.create_with_post(post_params)
 
     respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post.post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post.post }
+      if @entry.save
+        format.html { redirect_to @entry.post, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @entry.post }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,22 +45,22 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/slug or /posts/slug.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post.post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post.post }
+      if @entry.update(post_params)
+        format.html { redirect_to @entry.post, notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @entry.post }
       else
         format.html {
           set_all_tags
           render :edit, status: :unprocessable_entity
         }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /posts/slug or /posts/slug.json
   def destroy
-    @post.destroy
+    @entry.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
@@ -70,7 +70,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.with_rich_text_body.find_by(slug: params[:slug]).entry
+      @entry = Post.with_rich_text_body.find_by(slug: params[:slug]).entry
     end
 
     # Only allow a list of trusted parameters through.
@@ -80,19 +80,19 @@ class PostsController < ApplicationController
 
     # Notifies all links in the post about this published post.
     def webmention
-      return unless @post.published? && @post.post.body.saved_changes?
-      return if @post.changed? # changes weren't saved
-      source = post_url @post.post
+      return unless @entry.published? && @entry.post.body.saved_changes?
+      return if @entry.changed? # changes weren't saved
+      source = post_url @entry.post
 
       # Stores notified URIs, so that a page is not notified twice.
       posted_uris = Set.new
 
       # Notify every link
-      webmention_a_body source, posted_uris, @post.post.body
+      webmention_a_body source, posted_uris, @entry.post.body
 
       # Notify also removed links
-      if @post.post.body.body_previously_changed? && !@post.post.body.body_previously_was.nil?
-        webmention_a_body source, posted_uris, @post.post.body.body_previously_was
+      if @entry.post.body.body_previously_changed? && !@entry.post.body.body_previously_was.nil?
+        webmention_a_body source, posted_uris, @entry.post.body.body_previously_was
       end
     end
 
