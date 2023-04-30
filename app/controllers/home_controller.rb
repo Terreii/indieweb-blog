4f2e1aff78484586efdaf8bbd04742094572ef3c
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-04-30 11:48:52 +0200

    Revert "skip loading entries on home for mastodon"
    This reverts commit 25a10a8f9585901808e27cb55fde1cfd8a99fdb2.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    unless request.user_agent&.include? "Mastodon"
-      @entries = Entry.published.with_entryables.limit 10
-    else
-      @entries = []
-    end
+    @entries = Entry.published.with_entryables.limit 10
   end
 end
