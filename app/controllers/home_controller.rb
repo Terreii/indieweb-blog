e11d186b00ee4c66d1f6aa2e865cf6785360718b
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-07-20 20:50:42 +0200

    add max-age to root-path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @entries = Entry.published.with_entryables.limit 10
-    fresh_when @entries, public: true, cache_control: { "s-maxage": 30.seconds, must_revalidate: true }
+    fresh_when @entries, public: true, cache_control: {
+      max_age: 60.seconds,
+      "s-maxage": 30.seconds,
+      must_revalidate: true
+    }
   end
 end
