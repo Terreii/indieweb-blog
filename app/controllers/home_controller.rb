9241a2a9fea9b8e26eb84a5f7e60d9458a0edef5
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-08-28 10:38:26 +0200

    maybe it is cache_control & update gems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
     @entries = Entry.published.with_entryables.limit 10
-    fresh_when @entries, public: true, cache_control: {
-      max_age: 10.seconds,
-      "s-maxage": 15.seconds,
-      must_revalidate: true
-    }
+    unless Rails.env.test?
+      fresh_when @entries, public: true, cache_control: {
+        max_age: 10.seconds,
+        "s-maxage": 15.seconds,
+        must_revalidate: true
+      }
+    end
   end
 end
--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -97,9 +97,7 @@ class PostsTest < ApplicationSystemTestCase
     assert_text title
     click_on "Christophers thoughts"
     assert_no_css "article[lang=de]"
-    using_wait_time 5 do
-      assert_css "article[lang=de]"
-    end
+    assert_css "article[lang=de]"
 
     assert_equal "german", Post.last.entry.language
   end
