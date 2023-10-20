d0853f4ea70b5fdb78f935d6e08a685dffd2181b
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-10-20 14:04:05 +0200

    reload full nav header in a turbo frame

--- test/controllers/admin/nav_controller_test.rb
+++ test/controllers/admin/nav_controller_test.rb
@@ -4,13 +4,13 @@ class Admin::NavControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get admin_nav_url
     assert_response :success
-    assert_select "turbo-frame#admin_nav span.nav__item", false
+    assert_select "turbo-frame#admin_nav span.nav__item", count: 10
   end
 
   test "should get index when logged in" do
     login
     get admin_nav_url
     assert_response :success
-    assert_select "turbo-frame#admin_nav span.nav__item", count: 3
+    assert_select "turbo-frame#admin_nav span.nav__item", count: 13
   end
 end
