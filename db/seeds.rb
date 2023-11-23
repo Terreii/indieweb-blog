480198c9d4932d777ad0e412cb57fdafc75a4917
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-11-23 18:37:14 +0100

    fix seed thumbnail attachment

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,10 +66,11 @@ Entry.create [
   }
 ]
 
-Post.first.thumbnail.attach(
+first_post = Post.first
+first_post.thumbnail.attach(
   io: File.open(
     Rails.root.join('test', 'fixtures', 'files', 'sample.jpg')
   ),
   filename: 'thumbnail.jpg'
 )
-Post.first.thumbnail.analyze
+first_post.thumbnail.reload.analyze
