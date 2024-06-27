86d9f5d40037bc7e47811c5a17a74e359f75815a
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-06-27 15:26:06 +0200

    update Capistrano versioin lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.18.0"
+lock "~> 3.19.0"
 
 set :application, "indieweb-blog"
 set :repo_url, "https://github.com/Terreii/indieweb-blog.git"
