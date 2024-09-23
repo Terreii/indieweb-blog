f9488bbe2f55c2a372459cd2285781fcdbbbffd2
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-09-23 17:22:38 +0200

    add GoodJob cron for CleanupSessionJob

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -5,4 +5,16 @@ Rails.application.configure do
   config.good_job.retry_on_unhandled_error = true
   config.good_job.queues = '*'
   config.good_job.max_threads = 2 # Change if I need more
+
+  # Enable cron enqueuing in this process
+  config.good_job.enable_cron = true
+
+  config.good_job.cron = {
+    session_cleanup: {
+      cron: "0 0 * * *",
+      class: "CleanupSessionJob",
+      description: "Delete all UserSessions, which are inactive longer then 31 days.",
+      enabled_by_default: -> { Rails.env.production? } # Only enable in production
+    }
+  }
 end
