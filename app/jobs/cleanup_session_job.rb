9a2c12681d398e652f7c59a76e1694f24a0668da
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-09-27 01:36:38 +0200

    fixed a user_session render bug

--- app/jobs/cleanup_session_job.rb
+++ app/jobs/cleanup_session_job.rb
@@ -3,6 +3,8 @@ class CleanupSessionJob < ApplicationJob
 
   # Deletes all outdated sessions (older then 31 days).
   def perform
-    UserSession.outdated.destroy_all
+    sessions = UserSession.outdated
+    logger.info "CleanupSessionJob: deleting #{sessions.count} sessions."
+    sessions.destroy_all
   end
 end
