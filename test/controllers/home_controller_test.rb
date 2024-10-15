fe805f367a762cc9070cc4f40e6cb1bd2f4cc7c7
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-10-15 15:04:48 +0200

    add fediverse:creator meta data

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get root_url
     assert_response :success
   end
 
-  test "should have all entry types" do
+  test 'should have all entry types' do
     get root_url
 
     post = entries(:first_post)
@@ -14,12 +14,16 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
 
     # Check if all types are tested
     # This will fail as soon as a new type is added to Entry
-    types = [bookmark, post].map do |entry|
-      entry.entryable_type
-    end
-    assert_equal Entry.types, types, "All Entryable types should be tested"
+    types = [bookmark, post].map(&:entryable_type)
+    assert_equal Entry.types, types, 'All Entryable types should be tested'
 
     assert_select "##{dom_id post} h1", text: post.title
-    assert_select "##{dom_id bookmark} h1", text: "Bookmarked: " + bookmark.title
+    assert_select "##{dom_id bookmark} h1", text: "Bookmarked: #{bookmark.title}"
+  end
+
+  test 'should have mastodon creator meta data' do
+    get root_url
+
+    assert_select "meta[name=\"fediverse:creator\"][content=\"#{Rails.configuration.x.accounts.dig :mastodon, :user}\"]"
   end
 end
